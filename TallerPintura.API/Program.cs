070cfa8e833aec6d1bb0af65ef43e03057605788
Ranleisy <ranfy1327@gmail.com>
2026-04-23 18:18:09 -0400

    feat: Program.cs - configuracion de servicios y conexion a base de datos

--- TallerPintura.API/Program.cs
+++ TallerPintura.API/Program.cs
@@ -1,41 +1,67 @@
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+using TallerPintura.Application.Contracts;
+using TallerPintura.Application.Mapping;
+using TallerPintura.Application.Services;
+using TallerPintura.Domain.Entities;
+using TallerPintura.Infrastructure.Contracts;
+using TallerPintura.Infrastructure.Repositories;
+using TallerPintura.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+// Conexion a base de datos
+builder.Services.AddDbContext<TallerPinturaDbContext>(o =>
+    o.UseSqlServer(builder.Configuration.GetConnectionString("MainConnection")));
+
+// CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder => builder.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader());
+});
+
+// Repositorios
+builder.Services.AddScoped<IRepository<Estudiante>, GenericRepository<Estudiante>>();
+builder.Services.AddScoped<IRepository<Obra>, GenericRepository<Obra>>();
+builder.Services.AddScoped<IRepository<Sesion>, GenericRepository<Sesion>>();
+builder.Services.AddScoped<IRepository<Exposicion>, GenericRepository<Exposicion>>();
+builder.Services.AddScoped<IRepository<SesionEstudiante>, GenericRepository<SesionEstudiante>>();
+builder.Services.AddScoped<IRepository<ExposicionObra>, GenericRepository<ExposicionObra>>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+// Servicios
+builder.Services.AddScoped<IEstudianteService, EstudianteService>();
+builder.Services.AddScoped<IObraService, ObraService>();
+builder.Services.AddScoped<ISesionService, SesionService>();
+builder.Services.AddScoped<IExposicionService, ExposicionService>();
+
+// AutoMapper
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+// Controllers
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+        options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+    });
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
+app.UseCors("AllowAllOrigins");
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
